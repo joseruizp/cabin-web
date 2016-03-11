3e4ddff8a171276400c345e95f37643dad516957
jose <joseruizp@gmail.com>
2016-03-10 19:30:19 -0500

    agregando ajax pa obteners pcs

--- cabin-web/src/main/resources/static/js/operator.js
+++ cabin-web/src/main/resources/static/js/operator.js
@@ -1,4 +1,23 @@
 $(function() {
+	var strUrl = window.location.protocol + "//" + window.location.host + "/cabin-web/equipo";	
+	console.log("Get pc" + strUrl);
+	$.ajax({
+		type: "GET",
+	    url:strUrl,
+	    crossDomain: true,
+	    dataType: "json",
+	    success: function (json) {
+	    	var pcs = json._embedded.equipo;
+	    	
+	    	console.log("Get pc success" + json);
+	    },
+	    error: function (xhr, status) {	    	
+	    	console.log("Error, su solicitud no pudo ser atendida");
+	    	fnOpenErrorDialog();
+	    }
+	});
+	
+	
     $(".computer-multiple").click(function() {
     	$( "#computerDialog" ).dialog({
     		  width: 700
