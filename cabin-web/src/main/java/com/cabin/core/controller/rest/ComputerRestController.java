904f2d948d5e973789f77fc190aff3c3fd925b98
jose <joseruizp@gmail.com>
2016-04-13 19:18:48 -0500

    rest controller para computer

---
+++ cabin-web/src/main/java/com/cabin/core/controller/rest/ComputerRestController.java
@@ -0,0 +1,26 @@
+package com.cabin.core.controller.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cabin.core.persistence.domain.Computer;
+import com.cabin.core.persistence.repository.ComputerRepository;
+
+@RestController
+public class ComputerRestController {
+
+	@Autowired
+	private ComputerRepository computerRepository;
+
+	@RequestMapping(value = "/get/computersByHeadquarter", method = RequestMethod.GET, produces = {
+			"application/json;charset=UTF-8" })
+	public List<Computer> getComputersByHeadquarter(@RequestParam(value = "id", required = true) Long id) {
+		System.out.println("headquarterId ::: " + id);
+		return computerRepository.findByHeadquarterId(id);
+	}
+}
--- cabin-web/src/main/java/com/cabin/core/persistence/repository/ComputerRepository.java
+++ cabin-web/src/main/java/com/cabin/core/persistence/repository/ComputerRepository.java
@@ -1,6 +1,9 @@
 package com.cabin.core.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.cabin.core.persistence.domain.Computer;
@@ -8,4 +11,5 @@ import com.cabin.core.persistence.domain.Computer;
 @RepositoryRestResource(collectionResourceRel = "equipo", path = "equipo")
 public interface ComputerRepository extends JpaRepository<Computer, Long> {
 
+	List<Computer> findByHeadquarterId(@Param("id") Long id);
 }
