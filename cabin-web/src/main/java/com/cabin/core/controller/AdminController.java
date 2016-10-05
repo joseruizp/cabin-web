949199bc08d2b9a43e6f883005cc5ea343edefaa
ErickGV <gonzales.erick@pucp.edu.pe>
2016-10-05 01:24:12 -0500

    Cambios para la opcion recarga Admin, Opeartor

--- cabin-web/src/main/java/com/cabin/core/controller/AdminController.java
+++ cabin-web/src/main/java/com/cabin/core/controller/AdminController.java
@@ -2,6 +2,9 @@ package com.cabin.core.controller;
 
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.cabin.core.enums.SessionEnum;
 import com.cabin.core.persistence.domain.Headquarter;
 import com.cabin.core.persistence.repository.HeadquarterRepository;
 
@@ -30,10 +34,18 @@ public class AdminController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		//materialService.findByCriteria("Hola");
+	public String home(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+        Long userId = (Long) session.getAttribute(SessionEnum.USER_ID.name());
+
+        //Headquarter headquarter = headquarterRepository.findByUserId(userId);
+        //session.setAttribute(SessionEnum.HEADQUARTER_ID.name(), headquarter.getId());
+        
+        logger.info("Client id is:'" + userId + "'");
+        //logger.info("Headquarter id is:'" + headquarter.getId() + "'");
+
+        model.addAttribute("adminId", session.getAttribute(SessionEnum.EMPLOYEE_ID.name()));
+        //model.addAttribute("headquarterId", headquarter.getId());
 		
 		return "admin";
 	}
