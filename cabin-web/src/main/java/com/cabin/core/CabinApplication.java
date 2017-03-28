8fb6ff45cc1174b557992243a3a0b0063d775db2
ERICKELME <ERICKELME@192.168.1.40>
2017-03-27 23:59:14 -0500

    Correccion para que inicie el servidor

--- cabin-web/src/main/java/com/cabin/core/CabinApplication.java
+++ cabin-web/src/main/java/com/cabin/core/CabinApplication.java
@@ -3,13 +3,14 @@ package com.cabin.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.velocity.VelocityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = VelocityAutoConfiguration.class)
 @EnableJpaRepositories
 @SpringBootApplication
 public class CabinApplication extends SpringBootServletInitializer {
